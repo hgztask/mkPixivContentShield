b36353fab787cc07316741a397c91f65d3285deb
寒鸽子 <2978778354@qq.com>
2025-10-31 22:05:17 +0800

    feat(config): 更新脚本猫链接并添加描述信息 - 将 scriptCat_js_url 更新为新地址 - 在 tamper_monkey.json 中添加详细的脚本描述信息

--- src/data/globalValue.ts
+++ src/data/globalValue.ts
@@ -9,8 +9,7 @@ export default {
     group_url: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=tFU0xLt1uO5u5CXI2ktQRLh_XGAHBl7C&authKey=KAf4rICQYjfYUi66WelJAGhYtbJLILVWumOm%2BO9nM5fNaaVuF9Iiw3dJoPsVRUak&noverify=0&group_code=876295632',
     //作者B站链接
     b_url: 'https://space.bilibili.com/473239155',
-    //todo 后续记得更新地址·当前脚本所在脚本猫地址
-    scriptCat_js_url: 'https://scriptcat.org/zh-CN/script-show-page/1029',
+    scriptCat_js_url: 'https://scriptcat.org/zh-CN/script-show-page/4523',
     //脚本开源地址
     github_url: 'https://github.com/hgztask/mkPixivContentShield',
     update_log_url: 'https://docs.qq.com/doc/DSnhjSVZmRkpCd0Nj'
