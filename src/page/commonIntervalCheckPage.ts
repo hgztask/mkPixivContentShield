916420679cee09542df7179c24890dfb8db8b819
寒鸽子 <2978778354@qq.com>
2025-11-08 15:46:36 +0800

    refactor(commonIntervalCheckPage):优化页面列表选择器逻辑 - 合并重复的选择器字符串为数组形式 - 添加新选择器适配新版改动 -重构选择器拼接逻辑，提高可维护性 - 更新注释说明各选择器的具体作用页面 - 调整代码结构使逻辑更清晰

--- src/page/commonIntervalCheckPage.ts
+++ src/page/commonIntervalCheckPage.ts
@@ -2,19 +2,17 @@ import shielding from "../shieldingModel/shielding";
 import {IntervalExecutor} from "../model/IntervalExecutor";
 import pageCommon from "./pageCommon";
 
-/**
- * 间隔检查部分页面的列表
- * 作用小说页面和发现页小说页面、编辑部的推荐作品页·小说类、大家的新作插画和漫画、约稿页面的列表
- * 分类小说页的列表
- */
-const intervalCheckTheListOfSomePagesListExecutor = new IntervalExecutor(async () => {
-    //第二段css为编辑部的推荐作品页·是小说类
-    //第三段css为小说页原创热门作品栏右侧查看全部按钮跳转过后的按分类推荐的小说页的列表
-    const list = await pageCommon.getListData(
-        'ul>li.sc-9a31dd94-0,ul>li.sc-f59b1b7b-8,ul>li.sc-fec4147b-4,.sc-1453b7f5-2.dvNJyE', false)
-    shielding.shieldingItemDecorated(list)
-}, {processTips: true, intervalName: '部分页面列表'})
+const selectors = [
+    'ul>li.sc-9a31d d94-0',
+//编辑部的推荐作品页·是小说类
+    'ul>li.sc-f59b1b7b-8',
+//为小说页原创热门作品栏右侧查看全部按钮跳转过后的按分类推荐的小说页的列表
+    'ul>li.sc-fec4147b-4',
+    'ul>li.sc-9111aad9-0',
+    'sc-1453b7f5-2.dvNJyE'
+];
 
+const selector = selectors.join(',');
 
 //通用间隔检查页模块
 export default {
@@ -38,6 +36,15 @@ export default {
     isThisPage(url: string) {
         return url.endsWith('//www.pixiv.net/novel')
     },
-
-    intervalCheckTheListOfSomePagesListExecutor
+    /**
+     * 间隔检查部分页面的列表
+     * 作用小说页面和发现页小说页面、编辑部的推荐作品页·小说类、大家的新作插画和漫画、约稿页面的列表
+     * 分类小说页的列表
+     * 不包括小说页正在举办的比赛
+     */
+    intervalCheckTheListOfSomePagesListExecutor: new IntervalExecutor(async () => {
+        const list = await pageCommon.getListData(
+            selector, false)
+        shielding.shieldingItemDecorated(list)
+    }, {processTips: true, intervalName: '部分页面列表'})
 }
